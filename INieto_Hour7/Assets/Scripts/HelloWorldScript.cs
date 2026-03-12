25770990bc7cf7d8c6a04c63459492fbc91a8f78
tubooron <208729569+tubooron@users.noreply.github.com>
2026-03-11 18:53:54 -0700

    Hour 7 Assignment
    - Currently on operators
    - Added scripts and assigned them to the main camera to test the console output

---
+++ INieto_Hour7/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class HelloWorldScript : MonoBehaviour
+{
+    // Use this for initialization 
+    void Start ()
+    {
+        print ("Hello World");
+    }
+    //Update is called once per frame
+    void Update ()
+    {
+
+    }
+}

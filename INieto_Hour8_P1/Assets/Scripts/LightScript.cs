6381a8379f16dd80f069e0fe46dd49656ff6f640
tubooron <208729569+tubooron@users.noreply.github.com>
2026-03-27 01:09:10 -0700

    Hour 8 Part 1 completed
    - Mouse moves the camera up/down/left/right
    - Light turns on and off when L is pressed

--- INieto_Hour8_P1/Assets/Scripts/LightScript.cs
+++ INieto_Hour8_P1/Assets/Scripts/LightScript.cs
@@ -11,22 +11,19 @@ public class LightScript : MonoBehaviour
     void Start()
     {
         lightComponent = GetComponent<Light>();
-        //lightComponent.type = LightType.Directional;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Code below tests to see if the key was pressed
         L_Pressed = Input.GetKeyDown(KeyCode.L);
-        if (L_Pressed)
-        {
-            Debug.Log("L pressed! Light off...");
-            //LightObj = GetCompo
-            lightComponent.intensity = Mathf.PingPong(Time.time, 0);
 
-        }
-        //else
-        //    lightComponent.intensity = 
-
+        // If L is pressed AND the light component is currentltly on, it will turn off the light
+        // Although, if L is pressesd and the light is off it will turn the light on
+        if (L_Pressed && lightComponent.enabled == true)
+            lightComponent.enabled = false;
+        else if (L_Pressed && lightComponent.enabled == false)
+            lightComponent.enabled = true;
     }
 }

fff587696b2f96641e1c540dbf1f7263e307af42
tubooron <208729569+tubooron@users.noreply.github.com>
2026-04-11 02:37:54 -0700

    Hour 8 and 21 Completed
    - Completed both hour 8 and 21 (including TIY)
    - Created the ToggleAudio()
    - Adjusted the original Code to activate if 'Z' is pressed
    - Reduced volume of the 2D audio
    - Everything is functional

---
+++ INieto_Hour8and21/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ToggleAudio();
+
+            //if (audioSource.isPlaying == true)
+            //    audioSource.Stop();
+            //else
+            //    audioSource.Play();
+        }
+    }
+
+    void ToggleAudio()
+    {
+        if (audioSource.isPlaying == true)
+            audioSource.Stop();
+        else
+            audioSource.Play();
+    }
+}

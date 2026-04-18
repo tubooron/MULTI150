d1e6a9557afa8ddd49766eaed2040f6889f2f66b
tubooron <208729569+tubooron@users.noreply.github.com>
2026-04-18 15:56:32 -0700

    Hour 9 progression
    - Added more files while following the exercise

---
+++ INieto_Hour9/Assets/Scenes/Hour9/Scripts/MotionScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script will handle the player being able to move the sphere with the arrow keys
+public class MotionScript : MonoBehaviour
+{
+    // variables will be created that will hold the inputs of the arrow keys
+    float keyUpDown;
+    float keyLeftRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

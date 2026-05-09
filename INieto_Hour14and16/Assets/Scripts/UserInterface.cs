de86a18089ce797e931b2109bfee0334220a51b6
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-09 01:48:22 -0700

    Hour 14 progress
    - Completed the Exercise and chapter for this hour

---
+++ INieto_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,15 @@
+/* 
+Imported Image: Sephiroth Background for Panel. (Changes to this background after the button is clicked)
+
+Colors: Changes the image color to red and increase the Alpha value to not be completely transparent
+
+Typeface: Bold, Italics, Underline, Uppercase, Drop-Shadow (For Title Text),
+
+FontSizes: Title, Subtitle, Start Game Button all had font size changes. On click, all the text is changed to display new text and the button text changes to display a different message
+
+Button Colors: Normal Color (Blue), Highlighted Color (Yellow), and Pressed Color (Red) were selected. The fade duration was changed to 0.26.
+
+Specific Text:
+
+Additional Changes:
+*/

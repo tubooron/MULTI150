377630ca453c3557996f325bdd497d73de4ed590
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-14 01:04:05 -0700

    Hour14 and 16 completed
    - After hour 16 was completed, Hour 14 Buttons were modified to open the Level using a button and that function is contained in a new button added to that scene to keep the original button

--- INieto_Hour14and16/Assets/ParticleEffect.cs
+++ INieto_Hour14and16/Assets/ParticleEffect.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+/*
+ Changes made make a unique Particle System/Particle Emitter
 
-public class ParticleEffect : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+1.) Changed the Sprite to a Bubble that is transparent 
+2.) Adjusted the curve for the Gravity Modifier
+3.) Size over lifetime was given a curve to so the beginning of it's lifetime it will be a standard size and gradually decreease in size until it "dies"
+4.) Rate over Time was increase
+5.) Adjusted the Shape particle settings (Increased Angle, Radius, Arc Mode/Spread/Speed, and Emitter shape
+6.) Enabled noise and adjusted parameters to give the Bubbles more randomness as they float in the air
+7.) Force over Lifetime has been enabled and has changeed parameters
+8.) Increased the amount of particles that are emitted
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+ */

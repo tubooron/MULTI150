2a7270d5215d89d0941c5f9cf56013505877d5ac
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-14 21:25:56 -0700

    Hour 14 and 16
    - Added a README which explains how to view each Canvas

---
+++ INieto_Hour14and16/Assets/README.cs
@@ -0,0 +1,9 @@
+/*
+ TIY for hour 14 was made inside the INieto_Hour14 Scene
+
+To see the TIY Canvas make sure when clicking the first Canvas object that the Canvas Check mark in the inspector is present (enabled)!
+
+To see the custom "Game" Title Screen disable the Canvas (remove checkmark in inspector) from the Canvas containing the Galaxy background and enable the check mark on the Second Canvas to show the "game screen"
+
+The first check mark found at the top of the inspector should hide that Canvas
+ */

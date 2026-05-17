623cd35c01d64c9f5d035337f27dfe0b25f157c5
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-17 02:16:04 -0700

    Hour 17 and 18 Completed
    - Used Triggers to play the animations within the "Any State"
    - All animations play with the correct assigned keys

--- INieto_Hour17and18/Assets/Scripts/LampAnimationControl.cs
+++ INieto_Hour17and18/Assets/Scripts/LampAnimationControl.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class LampAnimationControl : MonoBehaviour
 {
-    Animator anim; 
+    // Initializes Animator object we are going to be using to get a reference by in later code
+    Animator anim;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,21 @@ public class LampAnimationControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Plays the Color Change Animation when W is pressed
+        if(Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger("ColorChange");
+
+        // Plays the Scale Animation when A is Pressed
+        if(Input.GetKeyDown(KeyCode.A))
+            anim.SetTrigger("ScaleChange");
+
+        // Plays the Spinning Animation when S is pressed
+        if (Input.GetKeyDown(KeyCode.S))
+            anim.SetTrigger("RotateChange");
+
+        // Plays the Hover Animation when D is pressed
+        if (Input.GetKeyDown(KeyCode.D))
+            anim.SetTrigger("HoverChange");
     }
 }

a48e107d585a612c1cbbc69739532374abd1c9aa
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-19 22:08:44 -0700

    Started Hour 20
    - Created a new scene and deleted the default scene
    - Added the Scripts folder and placed the GameCritics script inside

---
+++ INieto_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,17 @@
+/*
+1.) Is the game too easy or hard?
+    
+
+2.) What changes would you do to make it easier or harder?
+    
+
+3.) What would make the game more appealing?
+    
+
+4.)What parts of the game are fun? 
+    
+
+5.) What parts of the game are tedious?
+    
+ 
+ */

5e53f1c4c5385f2f98266f17a3d5a0c7edcf8b02
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-22 07:14:54 -0700

    Hour 20 Completed
    - GameCritics script has been updated with the changes made to this assignment
    - Many changes included are within the Prefab
    - Added HazardLine texture from the internet to use on the obstacle prefab-
    - Changed Prefab objects from default shapes

--- INieto_Hour20/Assets/Scripts/GameCritic.cs
+++ INieto_Hour20/Assets/Scripts/GameCritic.cs
@@ -14,4 +14,18 @@
 5.) What parts of the game are tedious?
     There is nothing too different. Not much variety in the gameplay. That is not always bad, but in this case if someone was looking for a challenge this would not be too difficult. Another tedious aspect would be the camera angles. Sometimes you could miss some points because of the perspective not lining up exactly as you would think.
  
+
+GAME CHANGES:
+
+- Changed the sphere mesh to a Chest and editted the particle system 
+- Decreased Duration, 
+- Start lifetime Increased, 
+- Decreased Start Speed to create a unique effect
+- Start size was changed to have 2 random constants to have different sizes on each particle
+- Color over life time was made to blend between two colors (Yellow and Orange)
+- Shape of the emitter was changed into a cone and the Arc loops. Angle and radius have been adjusted, Location of the shape was adjusted
+- Size over lifetime curve was changed to have a starting value of 2 and the end value to be at half of the curve graph
+- Changed the Obstacle to a plus sign which holds a holographic golden ball and this new object slightly extend the obstacle
+- Empty GameObjects were added in these prefabs so each new object added has transforms that can be editted and will not distort
+- Added HazardLines Texture for the obstacle Prefab (Link: https://static.vecteezy.com/system/resources/previews/010/948/846/non_2x/warning-seamless-pattern-with-yellow-and-black-diagonal-stripes-warn-caution-background-yellow-and-black-lines-tape-hazard-caution-sign-seamless-texture-illustration-vector.jpg)
  */

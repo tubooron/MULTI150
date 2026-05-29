eba40c6f009643b4bf4157fa41bb53f870f326e7
tubooron <208729569+tubooron@users.noreply.github.com>
2026-05-29 01:21:01 -0700

    Final Adjustment
    - Added Modification.cs text to the UI scene
    - Changed the Button location in order to have the ability to click it
    - Title text location has been moved
    - Added a README that explains some information about swapping between PreFabs

---
+++ INieto_Final/Assets/README.cs
@@ -0,0 +1,6 @@
+/*
+    (Modifications.cs/GauntletRunner portion of the Final): Prefabs of what the GauntletGame was modified with can be located in the Prefabs and swapped out
+    
+    (New Pickups and Hazards portion of the Final): The PreFabs for the OiiaCat and Donut are also included, but have been changed in the GameManager
+
+ */
